5977a120a8679d96f91e76096c380cbe47b74ba0
posokhin-dmitry <79544402+Posokhin-Dmitry@users.noreply.github.com>
2021-12-21 17:41:31 +0200

    fix

--- src/views/Cast.js
+++ src/views/Cast.js
@@ -10,22 +10,28 @@ const Cast = ({ id }) => {
   }, [dataCast, id]);
 
   return (
-    <ul className="cast-list">
-      {dataCast.map(({ id, name, profile_path, character }) => (
-        <li key={id} className="cast-item">
-          <img
-            src={
-              profile_path
-                ? `https://image.tmdb.org/t/p/w300/${profile_path}`
-                : 'https://ya1.fastfilm.net/images/poster_none.png'
-            }
-            alt={name}
-          />
-          <h1>{name}</h1>
-          <h3>{`as ${character}`}</h3>
-        </li>
-      ))}
-    </ul>
+    <>
+      {dataCast.lenght > 0 ? (
+        <ul className="cast-list">
+          {dataCast.map(({ id, name, profile_path, character }) => (
+            <li key={id} className="cast-item">
+              <img
+                src={
+                  profile_path
+                    ? `https://image.tmdb.org/t/p/w300/${profile_path}`
+                    : 'https://ya1.fastfilm.net/images/poster_none.png'
+                }
+                alt={name}
+              />
+              <h1>{name}</h1>
+              <h3>{`as ${character}`}</h3>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <h1>Not found</h1>
+      )}
+    </>
   );
 };
 
